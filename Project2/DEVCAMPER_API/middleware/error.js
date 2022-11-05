4390391b47cd7dbc6d25a6661731b7287f510c17
Matheus Soares <matheussilvasoares43@gmail.com>
2022-11-05 20:39:05 -0300

    aula 28

--- Project2/DEVCAMPER_API/middleware/error.js
+++ Project2/DEVCAMPER_API/middleware/error.js
@@ -6,14 +6,26 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message
 
   //Log to console for dev
-  console.log(err.stack.red)
-  console.log(req.value)
+  console.log(err)
 
   //Mongoose bad ObjectID
   if (err.name === 'CastError') {
     const message = `Resource not found with id of ${err.value}`
 
     error = new ErrorResponse(message, 404)
+  }
+
+  //Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered'
+
+    error = new ErrorResponse(message, 400)
+  }
+
+  //Mongoose validation error
+  if (err.name === 'ValidatorError') {
+    const message = Object.values(err.errors).map(val = val.message)
+    error = new ErrorResponse(message, 400)
   }
 
   res.status(error.statusCode || 500).json({
