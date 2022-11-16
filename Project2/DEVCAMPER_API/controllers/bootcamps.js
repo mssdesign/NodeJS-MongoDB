53546377a70a3f3b9bb99402b182dba757fea3d1
Matheus Soares <matheussilvasoares43@gmail.com>
2022-11-16 20:14:26 -0300

    Aula 39

--- Project2/DEVCAMPER_API/controllers/bootcamps.js
+++ Project2/DEVCAMPER_API/controllers/bootcamps.js
@@ -26,7 +26,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`)
 
   //finding resource
-  query = Bootcamp.find(JSON.parse(queryStr))
+  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses')
 
   //Select fields (requisição: {{URL}}/api/v1/bootcamps?select=name,description)
   if (req.query.select) {
@@ -131,13 +131,15 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route   DELETE /api/v1/bootcamps/:id
 // @acess    Private (tem que mandar token)
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
+  const bootcamp = await Bootcamp.findById(req.params.id)
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     )
   }
+
+  bootcamp.remove(); //Aciona o middleware pois o "findByIdAndDelete" não aciona
 
   res.status(200).json({ success: true, data: {} })
 })
