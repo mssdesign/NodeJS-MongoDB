91b0a294364b8520731576cb4d546a3c0ccff18a
Matheus Soares <matheussilvasoares43@gmail.com>
2022-11-30 18:40:00 -0300

    43

--- Project2/DEVCAMPER_API/server.js
+++ Project2/DEVCAMPER_API/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const colors = require('colors')
+const fileupload = require('express-fileupload')
 const connectDB = require('./config/db')
 const errorHandler = require('./middleware/error')
 
@@ -36,6 +37,9 @@ const logger = (req, res, next) => {
 }
 
 app.use(logger)
+
+//File upload
+app.use(fileupload())
 
 //Montar rotas
 app.use('/api/v1/bootcamps', bootcamps)
