345dcb6c68eb9ab2ebb8446b96847ac797da683a
Matheus Soares <matheussilvasoares43@gmail.com>
2022-12-09 17:51:02 -0300

    aula 54

--- Project2/DEVCAMPER_API/seeder.js
+++ Project2/DEVCAMPER_API/seeder.js
@@ -12,17 +12,21 @@ mongoose.connect(process.env.MONGO_URI)
 //load models
 const Bootcamp = require('./models/Bootcamp')
 const Course = require('./models/Course')
+const User = require('./models/User')
 
 // Read Json files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'))
 
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'))
 
+const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'))
+
 //Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps)
         await Course.create(courses)
+        await User.create(users)
         console.log('Data Imported...'.green.inverse)
         process.exit()
     } catch (err) {
@@ -35,6 +39,7 @@ const deleteData = async () => {
     try {
         await Bootcamp.deleteMany()
         await Course.deleteMany()
+        await User.deleteMany()
         //DeleteMany sem parâmetros deleta tuto do banco
 
         console.log('Data Destroyed...'.red.inverse)
