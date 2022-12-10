4375a32fb8dd911c8a613d586a50bde6471b1b4e
Matheus Soares <matheussilvasoares43@gmail.com>
2022-12-10 20:04:25 -0300

    Aula 65

--- Project2/DEVCAMPER_API/routes/reviews.js
+++ Project2/DEVCAMPER_API/routes/reviews.js
@@ -1,5 +1,11 @@
 const express = require('express')
-const { getReviews, getReview, addReview, updateReview } = require('../controllers/reviews')
+const {
+  getReviews,
+  getReview,
+  addReview,
+  updateReview,
+  deleteReview,
+} = require('../controllers/reviews')
 
 const Review = require('../models/Review')
 
@@ -14,8 +20,13 @@ router
   .get(
     advancedResults(Review, { path: 'bootcamp', select: 'name description' }),
     getReviews
-  ).post(protect, authorize('user', 'admin'), addReview)
+  )
+  .post(protect, authorize('user', 'admin'), addReview)
 
-  router.route('/:id').get(getReview).put(protect, authorize('user', 'admin'), updateReview)
+router
+  .route('/:id')
+  .get(getReview)
+  .put(protect, authorize('user', 'admin'), updateReview)
+  .delete(protect, authorize('user', 'admin'), deleteReview)
 
 module.exports = router
