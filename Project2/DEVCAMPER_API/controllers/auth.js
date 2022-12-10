55fd3fc7084cbc9ab002d73f57140864531818ee
Matheus Soares <matheussilvasoares43@gmail.com>
2022-12-09 21:38:29 -0300

    aula 58

--- Project2/DEVCAMPER_API/controllers/auth.js
+++ Project2/DEVCAMPER_API/controllers/auth.js
@@ -1,5 +1,7 @@
+const crypto = require('crypto')
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
+const sendEmail = require('../utils/sendEmail')
 const User = require('../models/User')
 
 // @desc Register user
@@ -47,6 +49,34 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res)
 })
 
+// @desc Reset password
+// @route   PUT /api/v1/auth/resetpassword/:resettoken
+// @acess    Public
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+  //Get hashed token
+  const resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(req.params.resettoken)
+    .digest('hex')
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() }
+  })
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid token', 400))
+  }
+
+  //Set new password
+  user.password = req.body.password
+  user.resetPasswordToken = undefined
+  user.resetPasswordExpire = undefined
+  await user.save()
+
+  sendTokenResponse(user, 200, res)
+})
+
 //Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   //Create token
@@ -73,11 +103,11 @@ const sendTokenResponse = (user, statusCode, res) => {
 // @route   POST /api/v1/auth/me
 // @acess    Private
 exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id) 
+  const user = await User.findById(req.user.id)
 
   res.status(200).json({
     success: true,
-    data: user
+    data: user,
   })
 })
 
@@ -86,7 +116,7 @@ exports.getMe = asyncHandler(async (req, res, next) => {
 // @acess   Public
 exports.forgotPassword = asyncHandler(async (req, res, next) => {
   const user = await User.findOne({ email: req.body.email })
-  
+
   if (!user) {
     return next(new ErrorResponse('There is no user with that email', 404))
   }
@@ -96,8 +126,33 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
   await user.save({ validateBeforeSave: false })
 
+  //Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/auth/resetpassword/${resetToken}`
+
+  const message = `You are receiving this email to reset a password. Please make a PUT request to: \n\n ${resetUrl}`
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message,
+    })
+
+    res.status(200).json({ success: true, data: 'Email sent' })
+  } catch (err) {
+    console.log(err)
+    user.resetPasswordToken = undefined
+    user.resetPasswordExpire = undefined
+
+    await user.save({ validateBeforeSave: false })
+
+    return next(new ErrorResponse('Email coud not be sent'))
+  }
+
   res.status(200).json({
     success: true,
-    data: user
+    data: user,
   })
 })
